7806533a1d761fb1a45ed2b9848bc5443e1b3098
AJ-Tan <aldomin22@gmail.com>
2026-04-29 22:15:39 +0800

    Add ThreedotsIcon component and corresponding SVG; update icon styles

---
+++ Frontend/src/components/Icons/ThreedotsIcon.tsx
@@ -0,0 +1,12 @@
+import ThreedotsIconSVG from "../../assets/icons/three-dots-icon.svg?react";
+import "./iconStyles.css";
+
+function ThreedotsIcon() {
+  return (
+    <div className={`icon-container three-dots`}>
+      <ThreedotsIconSVG />
+    </div>
+  );
+}
+
+export default ThreedotsIcon;
