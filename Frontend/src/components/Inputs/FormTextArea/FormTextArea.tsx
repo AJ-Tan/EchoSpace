0bea690c1ca24e8de3839d2cf99626bd6a609da7
AJ-Tan <aldomin22@gmail.com>
2026-04-29 22:16:38 +0800

    Add FormTextArea component with styles; implement textarea input and error message display

---
+++ Frontend/src/components/Inputs/FormTextArea/FormTextArea.tsx
@@ -0,0 +1,53 @@
+import "./formTextAreaStyles.css";
+
+type FormTextAreaProps = {
+  id: string;
+  state: [string, React.Dispatch<React.SetStateAction<string>>];
+  errorMessage?: string[];
+  placeholder?: string;
+};
+
+function FormTextArea({
+  id,
+  state,
+  errorMessage = [],
+  placeholder = "",
+}: FormTextAreaProps) {
+  const [inputValue, setInputValue] = state;
+
+  return (
+    <div className="form-input-container">
+      <div
+        className={`form-input ${inputValue.length ? "with-input" : ""} ${errorMessage?.length > 0 ? "warning" : ""}`}
+      >
+        <textarea
+          name={id}
+          id={id}
+          onChange={(e) => setInputValue(e.target.value)}
+          placeholder={placeholder}
+          value={inputValue}
+        ></textarea>
+      </div>
+      {errorMessage?.map((msg) => {
+        return (
+          <span key={msg} className="warning-message">
+            <svg
+              aria-hidden="true"
+              fill="currentColor"
+              focusable="false"
+              width="16px"
+              height="16px"
+              viewBox="0 0 24 24"
+              xmlns="https://www.w3.org/2000/svg"
+            >
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
+            </svg>
+            {msg}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+
+export default FormTextArea;
